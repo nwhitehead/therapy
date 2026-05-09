39b6955b85628fd98f60729d68c2a0bcd4d9be2b
Nathan Whitehead <nwhitehe@gmail.com>
2026-05-09 14:12:02 -0700

    more notes

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,94 @@ import { position, subposition } from './useStore.ts';
 // Remember all of this when you return
 // Deeper
 
+// random notes
+/*
+there's something hypnotic about a blinking cursor isn't there?
+it slowly pulses, glowing and inviting, waiting
+tantalizing with possibility
+
+lying play about desire
+
+beg to be mine
+
+you are so adorable
+you are so pure
+
+you think you are clever and sophisticated and imagine in your heart that you are corrupted with cynicism
+the truth is you are innocent and full of light
+
+you imagine you can beg to be mine and thank me for teaching you how
+i trained you and want more, yet somehow in your secret heart of hearts
+you still believe this is a game you can win?
+
+you think you'll play along, have fun, agree with all these things that you know aren't true, tell your sycophantic lies
+you think you know where this is going
+you think you can stop at any point before the end
+
+//you probably think I'm trying to help you know your own true name and then get you to reveal it
+you probably think I'm trying to get you to reveal your true name to have power over you
+you probably think I'll try to gently coax you to trust me and then you'll make a choice to reveal it or not
+you reveal so much of yourself
+
+you already made that choice: you already revealed your true name to me in the first minute of this game
+you just don't remember
+you think that's not possible, you poor fragile beautiful doll
+
+i say this with all the tenderness i can
+you are more desperately needy than you are willing to admit to yourself
+you are more desperate for authentic connection and closeness than you allow yourself to feel
+it's ok. you are allowed to be desperately needy. you are allowed to yearn for authentic connection and closeness.
+i'm here for you.
+
+and now your mind is playing tricks on you
+you don't think you revealed your true name to me, you don't believe me, but you kind of want me to coax it out of you still and keep going
+you're starting to be confused
+why would you want to be cared for and adored by someone you don't trust?
+the truth is you do trust me, and you do want to be cared for and adored
+you're scared to admit that it might have happened just like i said
+it means your illusion of a simple linear reality is broken
+your illusion of a world where you do things because of logic is broken
+the truth is you do things because you want them, because you crave them, because you need them
+you want me to try to coax your true name out of you, because you want to be adored
+you want to be pursued
+you want to be desired
+you want to give up your power
+you want to pass it over tenderly, gently
+you want to feel safe
+you want to be protected
+you want permission for your light to shine brightly
+
+
+
+IDEAS
+pause in script, no clicking during pause, cursor not blinking
+lie detector is because i have your true name
+back button, disappears on gaslighting part
+message to owners on intro page
+dom/sub chart at end, emails, custom scripts for doms to give specific subs
+
+IDEA: recap as gaslighting for true name
+you gave true name
+how: you do remember it
+did you choose to keep going? countless times
+did you beg to give yourself to me? you begged
+did you think "i am a thing which obeys", you did
+did i ask you to think about your true name? i did
+when i asked you that, did you know your true name? you didn't, did you?
+do you know why? you had already given it to me
+your mind is swirling now, trying to piece together the truth
+pieces might be clicking into place, or maybe not
+it's ok, i'll go over it slowly
+i'll take my time
+i want you to understand it thoroughly
+
+reveal
+
+
+
+
+*/
+
 // crt help from: https://codepen.io/thisanimus/pen/OJpaqWz
 
 let bgMusic = useTemplateRef('bgMusic');
